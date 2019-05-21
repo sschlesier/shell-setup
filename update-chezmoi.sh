5e198393c6b5c61c0816c8b06278eb9bc8681616
ScottS <scott@schlesier.ca>
2019-05-21 00:07:48 -0700

    improve update-chezmoi.sh

--- update-chezmoi.sh
+++ update-chezmoi.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
-
+CMDIR=$HOME/src/shell-setup/.local/share/chezmoi
+rm -rf $CMDIR
+mkdir -p $CMDIR
 cd $HOME/.local/share/chezmoi
-git archive master | tar -x -C $HOME/src/shell-setup/.local/share/chezmoi
+git archive master | tar -x -C $CMDIR
