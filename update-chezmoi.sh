178013a8291a3faf42b1ab6661532bfd087df5e1
Scott S <scotts@iqmetrix.com>
2019-05-23 14:03:30 -0700

    windows env vars
    and improved update-chezmoi.sh

--- update-chezmoi.sh
+++ update-chezmoi.sh
@@ -1,6 +1,4 @@
 #! /bin/bash
-CMDIR=$HOME/src/shell-setup/.local/share/chezmoi
-rm -rf $CMDIR
-mkdir -p $CMDIR
-cd $HOME/.local/share/chezmoi
-git archive master | tar -x -C $CMDIR
+TARGET=$(realpath .local/share/chezmoi)
+find $TARGET -mindepth 1 -delete 
+chezmoi source archive master | tar -x -C $TARGET
