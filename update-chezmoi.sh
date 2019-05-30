6d00c201f7f19da3631071fb89083f90c94ff7f5
Scott S <scott@schlesier.ca>
2019-05-29 22:39:56 -0700

    reorg to ulb and home dirs

--- update-chezmoi.sh
+++ update-chezmoi.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-rm -rf .local/
-mkdir -p .local/share/chezmoi
-chezmoi source archive -- master | tar -x -C .local/share/chezmoi
+rm -rf home/.local/
+mkdir -p home/.local/share/chezmoi
+chezmoi source archive -- master | tar -x -C home/.local/share/chezmoi
