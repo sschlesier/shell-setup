14fa84363c98aa25860ced3baf83fb7dd0688f53
Scott S <scotts@iqmetrix.com>
2019-06-04 10:02:25 -0700

    tweak update-chezmoi.sh

--- update-chezmoi.sh
+++ update-chezmoi.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 rm -rf home/.local/
 mkdir -p home/.local/share/chezmoi
-chezmoi source archive -- master | tar -x -C home/.local/share/chezmoi
+chezmoi source -- archive master | tar -x -C home/.local/share/chezmoi
